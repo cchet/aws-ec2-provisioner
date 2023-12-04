691814ff58098d0f28c809309e0c55aedba0e14a
Thomas Herzog <thomas.herzog@ihet.at>
2023-12-04 17:16:47 +0100

    - Fixed README - Added tags to the defined resources - Added .env file for setting the required environment variables

--- bin/ec2-app.ts
+++ bin/ec2-app.ts
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
 import {Ec2Stack} from '../lib/ec2-stack';
 import configuration from '../config/configuration.json';
+import {App, Tags} from "aws-cdk-lib";
 
-const app = new cdk.App();
+const app = new App();
 new Ec2Stack(app, `${configuration.id}Stack`, {
     env: {
         account: configuration.account,
         region: configuration.region
     }
 });
+
+Tags.of(app).add('id', configuration.id);
+Tags.of(app).add('account', configuration.account);
+Tags.of(app).add('region', configuration.region);
+
 app.synth();
